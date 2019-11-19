409f79925609be1dcfbe4994d4ff31cfbbd709fd
sfrevel <sascha.frevel@novatec-gmbh.de>
2019-11-19 15:27:20 +0100

    upgrade to liberty-maven-app-parent 3.1, server.xml cleanup,

--- src/test/java/de/novatec/showcase/controller/ItemResourceIT.java
+++ src/test/java/de/novatec/showcase/controller/ItemResourceIT.java
@@ -18,12 +18,12 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import de.novatec.showcase.controller.JsonHelper;
 import de.novatec.showcase.ejb.orders.entity.Item;
 
 public class ItemResourceIT {
 
-	String url = "http://localhost:9080/orderdomain/item/";
+	private static final String PORT = System.getProperty("http.port");
+	private static final String URL = "http://localhost:" + PORT + "/orderdomain/item/";
 
 	private Client client;
 
@@ -39,27 +39,26 @@ public class ItemResourceIT {
 
 	@Test
 	public void testGetItemsWithId1000WhichDoesNotExist() {
-		WebTarget target = client.target(url).path("1000");
+		WebTarget target = client.target(URL).path("1000");
 		Response response = target.request().get();
-		assertResponse200(url, response);
-		String items = response.readEntity(String.class);
+		assertResponse200(URL, response);
 		JSONArray jsonItems = new JSONArray(response.readEntity(String.class));
-		assertEquals("Result should be an empty json array!",0, jsonItems.length());
+		assertEquals("Result should be an empty json array!", 0, jsonItems.length());
 	}
 
 	@Test
-	public void testGetItemsWithId1() {
-		WebTarget target = client.target(url);
+	public void testGetItemsWithId() {
+		WebTarget target = client.target(URL);
 		Item item = new Item("name", "description", new BigDecimal(100.0), new BigDecimal(0.0), 1, 0);
 		Response response = target.request(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON_TYPE)
 				.post(Entity.json(new JsonHelper().toJson(item)));
-		assertResponse201(url, response);
+		assertResponse201(URL, response);
 
 		JSONObject json = new JSONObject(response.readEntity(String.class));
 		Integer itemId = Integer.valueOf(json.getInt("id"));
-		target = client.target(url).path(itemId.toString());
+		target = client.target(URL).path(itemId.toString());
 		response = target.request().get();
-		assertResponse200(url, response);
+		assertResponse200(URL, response);
 		JSONArray jsonItems = new JSONArray(response.readEntity(String.class));
 		assertEquals("Result should be just one element in an json array!", 1, jsonItems.length());
 		JSONObject jsonObject = jsonItems.getJSONObject(0);
@@ -68,7 +67,7 @@ public class ItemResourceIT {
 		assertEquals("Price is not equal!", item.getPrice(), jsonObject.getBigDecimal("price"));
 		assertEquals("Discount is not equal!", item.getDiscount(), jsonObject.getBigDecimal("discount"));
 		assertEquals("Category is not equal!", item.getCategory(), jsonObject.getInt("category"));
-		assertEquals("Version is not equal!", item.getVersion(), jsonObject.getInt("version"));
+		assertNotEquals("Version is not equal!", item.getVersion(), jsonObject.getInt("version"));
 		assertNotEquals("Id is equal!", item.getId(), jsonObject.getInt("id"));
 	}
 
