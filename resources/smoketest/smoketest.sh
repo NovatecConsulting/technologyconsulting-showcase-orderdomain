2a432dda600d43e9e447fe6258f7536e23636d42
sfrevel <3075075+sfrevel@users.noreply.github.com>
2020-01-13 09:27:14 +0100

    use mackserver also in smoketest.sh

--- resources/smoketest/smoketest.sh
+++ resources/smoketest/smoketest.sh
@@ -1,3 +1,9 @@
+java -Dmockserver.initializationJsonPath=./data/init_expectations.json -jar ./lib/mockserver-netty-5.8.1-jar-with-dependencies.jar -serverPort 9090 -logLevel ERROR &
+#wait while mockserver is staring
+sleep 1
+
+#curl -X PUT http://localhost:9090/retrieve?format=json&type=logs
+
 curl -u admin:adminpwd --header "Content-Type: application/json" --request POST --data @data/customer.json http://localhost:9080/orderdomain/customer
 
 for i in {1..24}; do
@@ -8,3 +14,5 @@ curl -u admin:adminpwd --header "Content-Type: application/json" --request POST 
 
 curl -u testuser:pwd --header "Content-Type: application/json" --request PUT http://localhost:9080/orderdomain/customer/add_inventory/1
 curl -u testuser:pwd --header "Content-Type: application/json" --request PUT http://localhost:9080/orderdomain/customer/sell_inventory/1/2/1
+
+curl -X PUT "http://localhost:9090/stop" -H  "accept: */*"
