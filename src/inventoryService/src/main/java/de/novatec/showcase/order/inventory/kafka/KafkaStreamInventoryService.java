3ff985618b645ae5b31d533b8113037dcc6eac28
Hieu Vo <duyhieu.vo@notatec-gmbh.de>
2020-03-18 11:48:42 +0100

    Migrate inventory service to use avro record

--- src/inventoryService/src/main/java/de/novatec/showcase/order/inventory/kafka/KafkaStreamInventoryService.java
+++ src/inventoryService/src/main/java/de/novatec/showcase/order/inventory/kafka/KafkaStreamInventoryService.java
@@ -3,6 +3,12 @@ package de.novatec.showcase.order.inventory.kafka;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
+import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
+import io.confluent.kafka.streams.serdes.avro.GenericAvroSerde;
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericEnumSymbol;
+import org.apache.avro.generic.GenericRecord;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.connect.json.JsonDeserializer;
@@ -19,6 +25,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
 import java.util.Properties;
 
 @Configuration
@@ -33,12 +40,17 @@ public class KafkaStreamInventoryService {
     @Value("${kafka.topic.order}")
     private String topic;
 
+    @Value("${kafka.schemaregistry}")
+    private String schemaRegistry;
+
     @Bean
     public KafkaStreams kafkaStreams(){
         Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, applicationid);
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.Integer().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, GenericAvroSerde.class);
+        props.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistry);
         final KafkaStreams kafkaStreams = new KafkaStreams(kafkaStreamTopology(), props);
         kafkaStreams.start();
 
@@ -51,11 +63,11 @@ public class KafkaStreamInventoryService {
         Serde<JsonNode> jsonSerde = Serdes.serdeFrom(new JsonSerializer(), new JsonDeserializer());
         final StreamsBuilder streamsBuilder = new StreamsBuilder();
 
-        KStream<Integer, JsonNode> order = streamsBuilder.stream(topic, Consumed.with(Serdes.Integer(), jsonSerde));
-        KStream<Integer, JsonNode> updatedOrder = order.filter((key, value) -> notFulfilled(value))
+        KStream<Integer, GenericRecord> order = streamsBuilder.stream(topic);
+        KStream<Integer, GenericRecord> updatedOrder = order.filter((key, value) -> notFulfilled(value))
                                                        .mapValues(value -> updateStatus(value));
 
-        updatedOrder.to(topic, Produced.with(Serdes.Integer(),jsonSerde));
+        updatedOrder.to(topic);
 
         return streamsBuilder.build();
     }
@@ -64,18 +76,18 @@ public class KafkaStreamInventoryService {
         this.topic = topic;
     }
 
-    private boolean notFulfilled(JsonNode value){
-        ObjectNode order = (ObjectNode)value;
-        if(!order.get("status").asText().equals("FULFILLED")) {
+    private boolean notFulfilled(GenericRecord record){
+        GenericEnumSymbol status = (GenericEnumSymbol) record.get("status");
+        if(!status.toString().equals("FULFILLED")) {
             return true;
         }
         return false;
     }
 
-    private JsonNode updateStatus(JsonNode value){
-        ObjectNode order = (ObjectNode)value;
-        order.put("status", "FULFILLED");
-        return order;
+    private GenericRecord updateStatus(GenericRecord value){
+        Schema schema = value.getSchema();
+        value.put("status", new GenericData.EnumSymbol(schema.getField("status").schema(), "FULFILLED"));
+        return value;
     }
 
 }
--- src/inventoryService/src/test/java/de/novatec/showcase/order/inventory/kafka/KafkaStreamInventoryServiceTest.java
+++ src/inventoryService/src/test/java/de/novatec/showcase/order/inventory/kafka/KafkaStreamInventoryServiceTest.java
@@ -1,15 +1,18 @@
 package de.novatec.showcase.order.inventory.kafka;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
+import io.confluent.kafka.streams.serdes.avro.GenericAvroSerde;
+
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.generic.GenericRecordBuilder;
 
 import org.apache.kafka.common.serialization.IntegerDeserializer;
 import org.apache.kafka.common.serialization.IntegerSerializer;
+import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.connect.json.JsonDeserializer;
-import org.apache.kafka.connect.json.JsonSerializer;
 import org.apache.kafka.streams.TopologyTestDriver;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.StreamsConfig;
@@ -20,6 +23,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 import static org.junit.Assert.assertEquals;
@@ -27,9 +32,11 @@ import static org.junit.Assert.assertEquals;
 
 public class KafkaStreamInventoryServiceTest {
     private TopologyTestDriver testDriver;
-    private ObjectMapper objectMapper;
+    Serde<GenericRecord> serde;
 
     private String topic="test";
+
+    private static final String MOCK_SCHEMA_REGISTRY_URL = "mock://testurl";
 
     @Before
     public void setUp(){
@@ -41,26 +48,44 @@ public class KafkaStreamInventoryServiceTest {
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "test-stream-processor");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "dummy:1234");
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.Integer().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, GenericAvroSerde.class);
+        props.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, MOCK_SCHEMA_REGISTRY_URL);
+
         testDriver = new TopologyTestDriver(topology, props);
-        objectMapper = new ObjectMapper();
+
+        //Configure the test serde to use mock schema registry
+        serde = new GenericAvroSerde();
+        Map<String, String> config = new HashMap<>();
+        config.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, MOCK_SCHEMA_REGISTRY_URL);
+        serde.configure(config, false);
     }
 
     @After
     public void tearDown(){
         testDriver.close();
+        serde.close();
     }
 
     @Test
-    public void kafkaStreamTopology() throws JsonProcessingException {
-        TestInputTopic<Integer, JsonNode> inputTopic = testDriver.createInputTopic(topic, new IntegerSerializer(), new JsonSerializer());
-        String jsonString = "{ \"status\" : \"DEFERRED\", \"k2\" : \"v2\" }";
-        JsonNode order = objectMapper.readTree(jsonString);
-        inputTopic.pipeInput(1,order);
+    public void avroKafkaStreamTopology(){
+        String stringSchema = "{ \"type\": \"record\"," +
+                "\"name\": \"test\"," +
+                "\"fields\": [" +
+                "{\"name\": \"status\"," +
+                "\"type\": {\"type\": \"enum\"," +
+                "\"name\":\"OrderStatus\"," +
+                "\"symbols\":[\"DEFERRED\",\"PENDING_MANUFACTUR\",\"SHIPPED\",\"DELETED\",\"UNKNOWN\",\"FULFILLED\"]}}]}";
+        Schema avroSchema = new Schema.Parser().parse(stringSchema);
+        GenericRecordBuilder builder = new GenericRecordBuilder(avroSchema);
+        builder.set("status", new GenericData.EnumSymbol(avroSchema.getField("status").schema(), "DEFERRED"));
+        GenericData.Record record = builder.build();
 
-        TestOutputTopic<Integer, JsonNode> outputTopic = testDriver.createOutputTopic(topic, new IntegerDeserializer(), new JsonDeserializer());
-        ObjectNode output = (ObjectNode)outputTopic.readValue();
+        TestInputTopic<Integer, GenericRecord> inputTopic = testDriver.createInputTopic(topic, new IntegerSerializer(), serde.serializer());
+        inputTopic.pipeInput(1,record);
 
-        String status = output.get("status").asText();
+        TestOutputTopic<Integer, GenericRecord> outputTopic = testDriver.createOutputTopic(topic, new IntegerDeserializer(), serde.deserializer());
+        GenericRecord output = outputTopic.readValue();
+        String status = output.get("status").toString();
         assertEquals("FULFILLED",status);
     }
 }
