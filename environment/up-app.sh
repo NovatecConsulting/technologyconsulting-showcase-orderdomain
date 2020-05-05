b0e6e56ff4080c60af8d94ece342c31cf6f4205a
Hieu Vo <duyhieu.vo@notatec-gmbh.de>
2020-05-05 16:05:28 +0200

    Detach application Docker container.

--- environment/up-app.sh
+++ environment/up-app.sh
@@ -5,12 +5,12 @@ cd $(dirname ${BASH_SOURCE[0]})
 source ./docker-compose.sh
 popd > /dev/null
 
-function start_service_app () {
-    docker_compose_in_environment up --build $1
+function start_service () {
+    docker_compose_in_environment up --build -d $1
 }
 
 function start_app () {
-    start_service_app "order"
+    start_service "order"
 }
 
 if [ "${BASH_SOURCE[0]}" == "$0" ]; then
