19644ba0219506b0df8b5e4bb4828d2d49b531ce
Hieu Vo <duyhieu.vo@notatec-gmbh.de>
2020-05-26 12:17:53 +0200

    Configure different credentials for order service and inventory service. Each service now has its own Kafka ACL.

--- environment/up-and-config.sh
+++ environment/up-and-config.sh
@@ -11,10 +11,12 @@ echo 'Grant operation rights to `kafka-schema-registry` client to create and man
 ./run-clitools.sh kafka kafka-acls --bootstrap-server kafka:9092 --command-config ./admin/config/adminClient.properties --add --allow-principal User:kafka-schema-registry --operation ALL --group schema-registry #default group ID of schema registry Kafka client is 'schema-registry'
 echo -e '\n'
 
-echo 'Create Kafka topic `order` and grant operation rights on that topic to client `kafka-client`'
+echo 'Create Kafka topic `order` and grant operation rights on that topic to clients `kafka-order-service` and `kafka-inventory-service`'
 ./run-clitools.sh kafka kafka-topics --bootstrap-server kafka:9092 --create --topic order --partitions 6 --replication-factor 3 --command-config ./admin/config/adminClient.properties
-./run-clitools.sh kafka kafka-acls --bootstrap-server kafka:9092 --command-config ./admin/config/adminClient.properties --add --allow-principal User:kafka-client --operation Read --operation Write --topic order
-./run-clitools.sh kafka kafka-acls --bootstrap-server kafka:9092 --command-config ./admin/config/adminClient.properties --add --allow-principal User:kafka-client --operation All --resource-pattern-type prefixed --topic inventory-service --group inventory-service
+./run-clitools.sh kafka kafka-acls --bootstrap-server kafka:9092 --command-config ./admin/config/adminClient.properties --add --allow-principal User:kafka-order-service --operation Read --operation Write --topic order
+
+./run-clitools.sh kafka kafka-acls --bootstrap-server kafka:9092 --command-config ./admin/config/adminClient.properties --add --allow-principal User:kafka-inventory-service --operation Read --operation Write --topic order
+./run-clitools.sh kafka kafka-acls --bootstrap-server kafka:9092 --command-config ./admin/config/adminClient.properties --add --allow-principal User:kafka-inventory-service --operation All --resource-pattern-type prefixed --topic inventory-service --group inventory-service
 echo -e '\n'
 
 echo 'Run the application and inventory service'
